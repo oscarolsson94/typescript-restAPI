3a2a8c5af88441f659ab6eba538841b8c6f6e4e3
Oscar Olsson <oscar.olsson94@hotmail.com>
2022-02-02 21:12:15 +0100

    finished api with documentation

---
+++ config/default.ts
@@ -0,0 +1,11 @@
+export default {
+    port: 1337,
+    dbUri: 'mongodb://localhost:27017/typescript-rest-api',
+    saltWorkFactor: 10,
+    accessTokenTtl: '15m',
+    refreshTokenTtl: '30d',
+    accessTokenPrivateKey: ``,
+    accessTokenPublicKey: ``,
+    refreshTokenPrivateKey: ``,
+    refreshTokenPublicKey: ``,
+};
