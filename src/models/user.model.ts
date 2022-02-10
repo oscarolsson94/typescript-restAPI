fb7b3931d607e237fa64caa3f37185d541b49aa2
Oscar Olsson <oscar.olsson94@hotmail.com>
2022-02-10 19:15:27 +0100

    no need to await hashSync

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -34,7 +34,7 @@ userSchema.pre("save", async function (next) {
 
     const salt = await bcrypt.genSalt(config.get<number>("saltWorkFactor"));
 
-    const hash = await bcrypt.hashSync(user.password, salt);
+    const hash = bcrypt.hashSync(user.password, salt);
 
     user.password = hash;
 
